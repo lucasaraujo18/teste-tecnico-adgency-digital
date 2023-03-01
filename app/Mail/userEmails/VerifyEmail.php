b603ca05e603ee7a29da1ae5f1ddfde8f36985b4
Lucas Araújo <lucas-dev>
2023-03-01 02:31:21 -0300

    🚧  Starting to verify e-mail service

---
+++ app/Mail/userEmails/VerifyEmail.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class VerifyEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+        public $verifyCode;
+        public $user;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($verifyCode, $user)
+    {
+        $this->user = $user;
+        $this->verifyCode = $verifyCode;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('emails.user.send-mail-verify')
+            ->subject('Bem vindo(a)')
+            ->with([
+                'user' => $this->user,
+                'email' => $this->email,
+                'route' => route('createSolicitation'),
+            ]);
+    }
+}
