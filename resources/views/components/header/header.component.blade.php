2a17244398579703e21af4140eed6008e200636d
Lucas Araújo <lucas-dev>
2023-03-03 02:06:22 -0300

    ✨ Head component

---
+++ resources/views/components/header/header.component.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        {{-- CSRF Token --}}
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        {{-- Cdns --}}
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+
+        {{-- Scripts --}}
+        <script src="{{ asset('js/app.js') }}}}"></script>
+
+        {{-- Styles --}}
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+        <title>Laravel</title>
+    </head>
+
