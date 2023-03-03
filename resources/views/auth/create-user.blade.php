eba6a6fb73cbf7d9964ebb31402f4921f3ef3209
Lucas Araújo <lucas-dev>
2023-03-03 02:31:28 -0300

    ✨ Pass the auth views to a auth main component

--- resources/views/auth/create-user.blade.php
+++ resources/views/auth/create-user.blade.php
@@ -1,31 +1,34 @@
-<div class="card">
-    <div class="card-header">
-        <h5 class="card-title">Card title</h5>
+@extends('components.templates.auth-component')
+@section('content')    
+    <div class="card">
+        <div class="card-header">
+            <h5 class="card-title">Card title</h5>
+        </div>
+        <div class="card-body">
+            <form method="POST" action="{{ route('users.store') }}">
+                @csrf
+                <div class="form-group">
+                    <label for="name">Nome</label>
+                    <input id="name" name="name" type="text" class="form-control" placeholder="Informe seu Nome">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input id="email" name="email" type="text" class="form-control" placeholder="Informe seu e-mail">
+                </div>
+                <div class="form-group">
+                    <label for="password">Senha</label>
+                    <input id="password" name="password" type="password" class="form-control" placeholder="Informe sua senha">
+                </div>
+                <div class="form-group">
+                    <label for="password_confirmation">Confirme sua senha</label>
+                    <input id="password_confirmation" name="password_confirmation" type="password" class="form-control" placeholder="Confirme sua senha">
+                </div>
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="privacy_terms" name="privacy_terms" value="1">
+                    <label class="form-check-label" for="privacy_terms">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
     </div>
-    <div class="card-body">
-        <form method="POST" action="{{ route('users.store') }}">
-            @csrf
-            <div class="form-group">
-                <label for="name">Nome</label>
-                <input id="name" name="name" type="text" class="form-control" placeholder="Informe seu Nome">
-            </div>
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input id="email" name="email" type="text" class="form-control" placeholder="Informe seu e-mail">
-            </div>
-            <div class="form-group">
-                <label for="password">Senha</label>
-                <input id="password" name="password" type="password" class="form-control" placeholder="Informe sua senha">
-            </div>
-            <div class="form-group">
-                <label for="password_confirmation">Confirme sua senha</label>
-                <input id="password_confirmation" name="password_confirmation" type="password" class="form-control" placeholder="Confirme sua senha">
-            </div>
-            <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="privacy_terms" name="privacy_terms" value="1">
-                <label class="form-check-label" for="privacy_terms">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-    </div>
-</div>
+@endsection
--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,12 +1,15 @@
-<div class="card">
-    <p>Olá {{ Auth::user()->name }}, confirme seu e-mail para usar o nosso sistema</p>
+@extends('components.templates.auth-component')
+@section('content')    
+    <div class="card">
+        <p>Olá {{ Auth::user()->name }}, confirme seu e-mail para usar o nosso sistema</p>
 
-    <form method="POST" action="{{ route('user.verifyCode') }}">
-        @csrf
-        <div class="form-group">
-            <label for="verify_code">Código de Verificação</label>
-            <input id="verify_code" name="verify_code" type="text" class="form-control" placeholder="Informe seu Nome">
-        </div>
-        <button type="submit" class="btn btn-primary">Verificar</button>
-    </form>
-</div>
+        <form method="POST" action="{{ route('user.verifyCode') }}">
+            @csrf
+            <div class="form-group">
+                <label for="verify_code">Código de Verificação</label>
+                <input id="verify_code" name="verify_code" type="text" class="form-control" placeholder="Informe seu Nome">
+            </div>
+            <button type="submit" class="btn btn-primary">Verificar</button>
+        </form>
+    </div>
+@endsection
