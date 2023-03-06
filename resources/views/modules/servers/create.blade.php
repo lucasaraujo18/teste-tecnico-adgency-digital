bb10f6112d06bca0aca6a3d21e9ba160c8d4a3bd
Lucas Araújo <lucas-dev>
2023-03-06 18:32:34 -0300

    💬  Fixing the form texts

--- resources/views/modules/servers/create.blade.php
+++ resources/views/modules/servers/create.blade.php
@@ -7,13 +7,13 @@
                 @csrf
                 <div class="form-group">
                     <label for="name">Nome</label>
-                    <input id="name" name="name" type="text" class="form-control" placeholder="Informe seu e-mail" value="{{ old('name') }}">
+                    <input id="name" name="name" type="text" class="form-control" placeholder="Informe o nome" value="{{ old('name') }}">
                 </div>
                 <div class="form-group">
                     <label for="ip">Endereço IP</label>
-                    <input id="ip" name="ip" type="ip" class="form-control" placeholder="Informe sua senha" value="{{ old('ip') }}">
+                    <input id="ip" name="ip" type="ip" class="form-control" placeholder="Informe o endereço de ip" value="{{ old('ip') }}">
                 </div>
-                <button type="submit" class="btn form-button form-button-primary">Criar</button>
+                <button type="submit" class="btn form-button form-button-primary">Criar servidor</button>
             </form>
         </div>
     </div>
--- resources/views/modules/sites/create.blade.php
+++ resources/views/modules/sites/create.blade.php
@@ -1,19 +1,19 @@
 @extends('components.templates.main-component')
 @section('content')
     <div class="form-template">
-        <h5 class="form-title">Card title</h5>
+        <h5 class="form-title">Criar Site</h5>
         <form method="POST" action="{{ route('sites.store') }}">
             @csrf
             <input type="hidden" name="server_id" value="{{ $server }}">
             <div class="form-group">
                 <label for="name">Nome do site</label>
-                <input id="name" name="name" type="text" class="form-control" placeholder="Informe seu e-mail" value="{{ old('name') }}">
+                <input id="name" name="name" type="text" class="form-control" placeholder="Informe o nome" value="{{ old('name') }}">
             </div>
             <div class="form-group">
                 <label for="url">URL do site</label>
-                <input id="url" name="url" type="url" class="form-control" placeholder="Informe sua senha" value="{{ old('url') }}">
+                <input id="url" name="url" type="url" class="form-control" placeholder="Informe a url" value="{{ old('url') }}">
             </div>
-            <button type="submit" class="btn form-button form-button-primary">Submit</button>
+            <button type="submit" class="btn form-button form-button-primary">Criar site</button>
         </form>
     </div>
 @endsection
