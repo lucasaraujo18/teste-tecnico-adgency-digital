3023eb9a6c6d908347c94e89ce2ebd02f420386f
Lucas Araújo <lucas-dev>
2023-03-06 21:16:16 -0300

    💄  Verify email

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,12 +1,13 @@
 @extends('components.templates.auth-component')
 @section('auth-content')    
     <div class="form-template">
+        <h5 class="form-title">Verificar E-mail</h5>
         <label>Olá {{ Auth::user()->name }}, confirme seu e-mail para usar o nosso sistema</label>
         <form method="POST" action="{{ route('user.verifyCode') }}">
             @csrf
             <div class="form-group">
                 <label for="verify_code">Código de Verificação</label>
-                <input id="verify_code" name="verify_code" type="text" class="form-control" placeholder="Informe seu Nome">
+                <input id="verify_code" name="verify_code" type="text" class="form-control" placeholder="Informe o código">
             </div>
             <button type="submit" class="btn form-button form-button-primary">Verificar</button>
         </form>
